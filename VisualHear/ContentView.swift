5725b37275b132a34af7beb902607b1f4c6ace11
John Towery <kbaries@gmail.com>
2023-05-10 20:24:31 -0600

    Initial Commit

---
+++ VisualHear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisualHear
+//
+//  Created by John Towery on 5/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisualHear/VisualHearApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualHearApp.swift
+//  VisualHear
+//
+//  Created by John Towery on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VisualHearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
